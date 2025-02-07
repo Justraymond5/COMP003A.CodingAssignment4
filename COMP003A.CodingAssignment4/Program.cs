787c7ffdf18fda92aabe2cef7768cc2df90cdc28
raymondgonzalez <collegeeduraymond@gmail.com>
2025-02-06 20:28:22 -0800

    Connecting to Git

---
+++ COMP003A.CodingAssignment4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace COMP003A.CodingAssignment4;
+// Author: Raymond Gonzalez
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Inventory management application with a minimum of 10 elements in the collection.
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Intro
+        Console.WriteLine("Welcome to the Inventory Management System!");
+        Console.WriteLine("Choose data storage type:");
+        Console.WriteLine("1. Arrays");
+        Console.WriteLine("2. Lists");
+        Console.Write("Enter your choice: ");
+        int theirChoice =int.Parse(Console.ReadLine());
+        //End Intro
+        
+            
+    }
+}
