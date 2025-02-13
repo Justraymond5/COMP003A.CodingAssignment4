9edfced339f8463bff2eb2d9c417cde2b7e32085
raymondgonzalez <collegeeduraymond@gmail.com>
2025-02-12 23:07:09 -0800

    UPDATED CODE: Added Methods fully, finally

--- COMP003A.CodingAssignment4/Program.cs
+++ COMP003A.CodingAssignment4/Program.cs
@@ -6,6 +6,9 @@
 class Program
 {
     //Method for storing main into
+    /// <summary>
+    /// This is just a short into about what type of system will be used to show it when called
+    /// </summary>
     static void Into()
     {
         Console.WriteLine("Welcome to the Inventory Management System!");
@@ -13,6 +16,9 @@ class Program
         Console.WriteLine("Please enter '1' to proceed using  Arrays");  
     }
     // Method for storing menu
+    /// <summary>
+    /// Storing the menu in a method to show it when called
+    /// </summary>
     static void Menu()
     {
         Console.WriteLine("\n");
@@ -23,7 +29,124 @@ class Program
         Console.WriteLine("4. Exit");
         Console.Write("\nEnter your choice: ");  
     }
-    
+    //Method for Part 1: Add
+    //Notes:Syntax; type of variable, string. parameter (), needs to have value of the variables for each type of variable, ex string and int
+    /// <summary>
+    /// Used a string method to store text, more spescifually input text from the user and save it into an array
+    /// </summary>
+    /// <param name="productNames">Stores names of the product</param>
+    /// <param name="productQuantities">Stores quantities of the product</param>
+    /// <param name="count">Keeps track of how many items</param>
+    /// <returns>A weak catch all, telling the user they did something wrong so they would have to try again</returns>
+    static string addingInfo( ref string[] productNames, ref int[] productQuantities, ref int count)
+    {
+        while (true) //Cycle through inventroyChoice 1 until everything is good to move on
+        {
+            if (count < 10) //Check if they break the bagpack
+            {
+                try
+                {
+                    Console.Write("Enter product name: ");
+                    string productName = Console.ReadLine();
+                    Console.Write("Enter product quantity: ");
+                    int productQuantity = int.Parse(Console.ReadLine());
+                    //Console.Write("Product added successfully!");
+                    
+                            
+                    // (Open) Stores the product info in the arrays
+                    productNames[count] = productName;
+                    productQuantities[count] = productQuantity;
+                    count++;
+                    // (Close) Stores the product info in the arrays
+                    return "Product added successfully!";        
+                    
+                }
+                    catch (FormatException )
+                    {
+                        //Console.WriteLine("You did something wrong!");
+                        return "You did something wrong!";
+                    }   
+            }
+            else
+            {
+                //Console.WriteLine("Backpack is full, cant add more than 10 items");
+                //break;
+                return "Backpack is full, cant add more than 10 items";
+            }
+                        
+        }
+    }
+    //Notes: Same syntax as above
+    /// <summary>
+    /// Allowing to change the valuses saved in the array
+    /// </summary>
+    /// <param name="productNames"> Name of product</param>
+    /// <param name="productQuantities">Number of quantites</param>
+    /// <param name="count">keeps track of number of products and quantites </param>
+    /// <returns> Error message to try again or a good job message to then push the menu again</returns>
+    static string Update( ref string[] productNames, ref int[] productQuantities, ref int count)
+    {
+        Console.WriteLine("Update Product Quantity");
+        Console.Write("\nEnter product name: ");
+        string updateNameProduct = Console.ReadLine();
+        int index = Array.IndexOf(productNames, updateNameProduct, 0, count); //Cyles through the array "string folder" in search for updateNameProduct starting from 0 
+                    
+        if (index != -1) //if it finds the product, good if not bad so it loops back
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter new quantity: ");
+                    int newQuantity = int.Parse(Console.ReadLine());
+                    productQuantities[index] = newQuantity; // Update product quantity after valid find of product name
+                    //Console.WriteLine("Product quantity updated successfully!");
+                    return "Product updated successfully!";
+                   
+                }
+                catch (FormatException)
+                {
+                    //Console.WriteLine("Please enter a valid new quantity");
+                    return "Please enter a valid new quantity";
+                }
+            }
+        }
+        else
+        {
+            
+            //Console.WriteLine("Please enter a valid product name!");
+            return "Please enter a valid product name!";
+        }  
+    }
+    /// <summary>
+    /// A void method to avoid a need for return, used to display a calculation
+    /// </summary>
+    /// <param name="productNames">Store productNames </param>
+    /// <param name="productQuantities">Store productQuantities</param>
+    /// <param name="count">Keep track of number of items (names and quantites)</param>
+    static void inventoryMath( ref string[] productNames, ref int[] productQuantities, ref int count)
+    {
+        Console.Write("\nInventory Summary: ");
+        Console.WriteLine("\n ");
+        int totalQuantity = 0;
+        for (int i = 0; i < count; i++) // WriteLine's the stored products
+        {
+            Console.WriteLine($"- {productNames[i]}: {productQuantities[i]}");
+            totalQuantity += productQuantities[i];
+        }
+
+        double sum = 0;
+        for (int i = 0; i < productQuantities.Length; i++)
+        {
+            sum += productQuantities[i];
+        }
+        // Calculation for average
+        double average = (count > 0) ? sum / count : 0;
+        Console.WriteLine($"\nTotal Products: {count} ");
+        Console.WriteLine($"Total Quantity: {totalQuantity} ");
+        Console.WriteLine($"Average Quantity: {average:F2}");  
+        
+    }
     static void Main(string[] args)
     {
         //Storage Arrays
@@ -75,90 +198,20 @@ class Program
                 int inventoryChoice = int.Parse(Console.ReadLine());
                 if (inventoryChoice == 1)
                 {
-                    while (true) //Cycle through inventroyChoice 1 until everything is good to move on
-                    {
-                        if (count < 10) //Check if they break the bagpack
-                        {
-                            try
-                            {
-                                Console.Write("Enter product name: ");
-                                string productName = Console.ReadLine();
-                                Console.Write("Enter product quantity: ");
-                                int productQuantity = int.Parse(Console.ReadLine());
-                                Console.Write("Product added successfully!");
-                            
-                                // (Open) Stores the product info in the arrays
-                                productNames[count] = productName;
-                                productQuantities[count] = productQuantity;
-                                count++;
-                                // (Close) Stores the product info in the arrays
-                            
-                                break;
-                            }
-                            catch (FormatException )
-                            {
-                                Console.WriteLine("You did something wrong!");
-                            }   
-                        }
-                        else
-                        {
-                            Console.WriteLine("Backpack is full, cant add more than 10 items");
-                            break;
-                        }
-                        
-                    }
+                    //Syntax: Remove the variable types
+                    Console.WriteLine(addingInfo(ref productNames, ref productQuantities, ref count));
                 }
                 else if (inventoryChoice == 2)
                 {
-                    Console.WriteLine("Update Product Quantity");
-                    Console.Write("\nEnter product name: ");
-                    string updateNameProduct = Console.ReadLine();
-                    int index = Array.IndexOf(productNames, updateNameProduct, 0, count); //Cyles through the array "string folder" in search for updateNameProduct starting from 0 
+                    //Syntax: Same as above
+                    Console.WriteLine(Update( ref productNames, ref productQuantities, ref count));
                     
-                    if (index != -1) //if it finds the product, good if not bad so it loops back
-                    {
-                        while (true)
-                        {
-                            try
-                            {
-                                Console.Write("Enter new quantity: ");
-                                break;
-                            }
-                            catch (FormatException)
-                            {
-                                Console.WriteLine("Please enter a valid new quantity");
-                            }
-                        }
-                        int newQuantity = int.Parse(Console.ReadLine());
-                        productQuantities[index] = newQuantity; // Update product quantity after valid find of product name
-                        Console.WriteLine("Product quantity updated successfully!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please enter a valid product name!");
-                    }
                 }
                 else if (inventoryChoice == 3)
                 {
-                    Console.Write("\nInventory Summary: ");
-                    Console.WriteLine("\n ");
-                    int totalQuantity = 0;
-                    for (int i = 0; i < count; i++) // WriteLine's the stored products
-                    {
-                        Console.WriteLine($"- {productNames[i]}: {productQuantities[i]}");
-                        totalQuantity += productQuantities[i];
-                    }
-
-                    double sum = 0;
-                    for (int i = 0; i < productQuantities.Length; i++)
-                    {
-                        sum += productQuantities[i];
-                    }
-                    // Calculation for average
-                    double average = (count > 0) ? sum / count : 0;
-                    Console.WriteLine($"\nTotal Products: {count} ");
-                    Console.WriteLine($"Total Quantity: {totalQuantity} ");
-                    Console.WriteLine($"Average Quantity: {average:F2}");
+                    //Syntax: Same as above
+                    inventoryMath(ref productNames, ref productQuantities, ref count);
+                    
                 }
                 else if (inventoryChoice == 4)
                 {
@@ -190,7 +243,15 @@ class Program
     
 }    
 //Notes of Assignment
+
 //For the arrays I used an integer 'count' variable to track how many items have been added. Ensure the count does not exceed the array's size.
 //Hint Arrays: Use Array.IndexOf() to find the index of the product name.
 // Hint  Lists: Use .IndexOf() to find the index of the product name.
 //For list it was .Add()
+
+//When using Methods ->> it saves strings of code in a differnt world
+//When you need to use it just call for its name
+//The name is the name of the new folder which is your method name
+//If it has peramiters have the name of the peramiters aswell.
+// A paramerter is just like the name of the action that methond will run
+// The ref key is just collecting the value, but it needs to be called too with that same ref
